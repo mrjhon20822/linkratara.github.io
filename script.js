39736626f04104b8c9124af92eab0938c18abb9c
Mrjhon Mc <jhonycraft125@gmail.com>
2026-03-25 10:27:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function copyIP() {
+  const ip = "play.shadowyn.id"; // GANTI IP
+
+  navigator.clipboard.writeText(ip).then(() => {
+    showNotif();
+  });
+}
+
+function showNotif() {
+  const notif = document.getElementById("notif");
+
+  notif.classList.add("show");
+
+  setTimeout(() => {
+    notif.classList.remove("show");
+  }, 2500);
+}
