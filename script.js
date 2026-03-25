98c5e2a2e7101ce6cec8c8e0c329b6eba2e3daf6
Mrjhon Mc <jhonycraft125@gmail.com>
2026-03-25 12:17:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 /* =========================
+   LOAD COMPONENT (NAVBAR, SIDEBAR, FOOTER)
+========================= */
+function loadComponent(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("navbar", "navbar.html");
+  loadComponent("sidebar-container", "sidebar.html");
+  loadComponent("footer", "footer.html");
+});
+
+
+/* =========================
    COPY IP + NOTIF
 ========================= */
 function copyIP() {
@@ -16,9 +34,10 @@ function copyIP() {
 function showNotif(text) {
   const notif = document.getElementById("notif");
 
-  if (!notif) return; // biar gak error kalau notif gak ada
+  if (!notif) return;
 
-  notif.querySelector("span").innerText = text;
+  const span = notif.querySelector("span");
+  if (span) span.innerText = text;
 
   notif.classList.add("show");
 
@@ -26,6 +45,7 @@ function showNotif(text) {
     notif.classList.remove("show");
   }, 2500);
 }
+
 
 /* =========================
    SIDEBAR
@@ -40,13 +60,13 @@ function toggleSidebar() {
   overlay.classList.toggle("active");
 }
 
+
 /* =========================
    THEME (LIGHT / DARK)
 ========================= */
 function toggleTheme() {
   document.body.classList.toggle("light-mode");
 
-  // optional: ganti icon 🌙 ↔ ☀
   const icon = document.querySelector(".nav-btn i");
 
   if (icon) {
@@ -58,7 +78,33 @@ function toggleTheme() {
       icon.classList.add("fa-moon");
     }
   }
+
+  // SIMPAN KE LOCAL STORAGE
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
 }
+
+
+/* =========================
+   LOAD THEME SAAT BUKA WEB
+========================= */
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light") {
+    document.body.classList.add("light-mode");
+
+    const icon = document.querySelector(".nav-btn i");
+    if (icon) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    }
+  }
+});
+
 
 /* =========================
    AUTO CLOSE SIDEBAR (klik overlay)
